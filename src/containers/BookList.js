f9ed31d0b82ec7e3d7a6b1ad4e7a6363c9227d75
janis-zalitis-moby <janis.zalitis@edu2x.com>
2017-09-17 19:54:39 +0300

    refactor: Made various improvements to functionality
    After retesting found various parts I wanted to improve
    or could be perceived as bugs and improved them:
     - moved out the shelve list into a config file as include
     - reduced BookShelf signature as 1 param was not needed
     - used state.loading/loaded to display proper message in search
     - used debounce from throttle-debounce to prevent multiple API calls
     - moved books variable to App.state
     - moved a couple of reused methods to App
     - streamlined Booklist to list shelves from BookShelvesConfig
     - used prop-types package instead of React.Proptypes

--- src/containers/BookList.js
+++ src/containers/BookList.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import * as BooksAPI from './../BooksAPI';
 import './../App.css';
 
 import BookShelf from '../components/BookShelf';
+import BookShelves from '../components/BookShelvesConfig';
 
 /** 
  * Outputs a page with a list of books split by shelves
@@ -14,67 +15,36 @@ import BookShelf from '../components/BookShelf';
  *  { read: "Read" }
  * ]
  */
-class BookList extends Component {
-  state = {
-    books: [],
-  }
-
-  componentDidMount = () => this.updateBookData();
-
-  /** 
-   * Moves book to target shelf
-   * @param  {object} book  book
-   * @param  {string} shelf shelf(.value)
-   */
-  onChangeShelf = (book, shelf) =>
-    BooksAPI.update(book, shelf).then(
-      () => this.updateBookData()
-    );
-
-  /**
-   * Re-fetches book data from API
-   */
-  updateBookData = () =>
-    BooksAPI.getAll().then(
-      data => this.setState({ books: data })
-    );
-
-  render() {
-    const { books } = this.state;
-
-    return (
-      <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-        {books.length ?
-          <div className="list-books-content">
-            <div>
-              <BookShelf
-                title="Currently Reading"
-                books={books.filter(book => book.shelf === 'currentlyReading')}
-                onChangeShelf={this.onChangeShelf}
-              />
-              <BookShelf
-                title="Want to Read"
-                books={books.filter(book => book.shelf === 'wantToRead')}
-                onChangeShelf={this.onChangeShelf}
-              />
-              <BookShelf
-                title="Read"
-                books={books.filter(book => book.shelf === 'read')}
-                onChangeShelf={this.onChangeShelf}
-              />
-            </div>
-          </div>
-          : null
-        }
-        <div className="open-search">
-          <Link to="/search">Search</Link>
-        </div>
+const BookList = ({ books, onChangeShelf }) => 
+  (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>MyReads</h1>
       </div>
-    );
-  }
-}
+      {books.length ?
+        <div className="list-books-content">
+          <div>
+            {BookShelves.filter(s => !s.hideShelf).map(s => (
+               <BookShelf
+                 title={s.name}
+                 key={s.key}
+                 books={books.filter(book => book.shelf === s.key)}
+                 onChangeShelf={onChangeShelf}
+               />
+            ))}
+          </div>
+        </div>
+        : null
+      }
+      <div className="open-search">
+        <Link to="/search">Search</Link>
+      </div>
+    </div>
+  );
+
+BookList.propTypes = {
+  books: PropTypes.array.isRequired,
+  onChangeShelf: PropTypes.func.isRequired,
+};
 
 export default BookList;
